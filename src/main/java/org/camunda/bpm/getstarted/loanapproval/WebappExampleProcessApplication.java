da4624aaab0d1462cb6c222d1630af2ba0aac087
Svetlana Dorokhova <svetlana.dorokhova@camunda.com>
2017-09-28 11:05:51 +0200

    Step 1: setup a Spring Boot application project

---
+++ src/main/java/org/camunda/bpm/getstarted/loanapproval/WebappExampleProcessApplication.java
@@ -0,0 +1,11 @@
+package org.camunda.bpm.getstarted.loanapproval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappExampleProcessApplication {
+	public static void main(String... args) {
+		SpringApplication.run(WebappExampleProcessApplication.class, args);
+	}
+}
